ba6e79eccb1543acf6ab565bcdceaa21a473fbb7
Alexandre Mutti <alexmutti@gmail.com>
2020-04-04 17:41:39 -0300

    defstruct and create_account method

--- lib/stn_account.ex
+++ lib/stn_account.ex
@@ -3,16 +3,29 @@ defmodule StnAccount do
   Documentation for StnAccount.
   """
 
+  defstruct [:tax_id, :balance, :transactions]
+
+  @type t :: %__MODULE__{
+    tax_id: String.t(),
+    balance: Money.t(),
+    transactions: []
+  }
+
   @doc """
-  Hello world.
+  Creates a new BRL account.
 
   ## Examples
 
-      iex> StnAccount.hello()
-      :world
+    iex> StnAccount.create("85718312036")
 
   """
-  def hello do
-    :world
+  def create(tax_id) do
+    unless is_binary(tax_id) && String.length(tax_id) == 11 do
+      raise(ArgumentError, message: "Invalid CPF.")
+    end
+
+    balance = Money.new(0, :BRL)
+
+    %{tax_id: tax_id, balance: balance, transactions: []}
   end
 end
