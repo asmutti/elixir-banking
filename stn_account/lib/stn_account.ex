6ed002c9f7642922ae3a7b7c383694d252f5406f
Alexandre Mutti <alexmutti@gmail.com>
2020-04-04 13:47:55 -0300

    init project

---
+++ stn_account/lib/stn_account.ex
@@ -0,0 +1,18 @@
+defmodule StnAccount do
+  @moduledoc """
+  Documentation for StnAccount.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StnAccount.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ stn_account/test/stn_account_test.exs
@@ -0,0 +1,8 @@
+defmodule StnAccountTest do
+  use ExUnit.Case
+  doctest StnAccount
+
+  test "greets the world" do
+    assert StnAccount.hello() == :world
+  end
+end
