90495d515de1ede6d1e93b13fae6ca8531b3e4e6
Alexandre Mutti <alexmutti@gmail.com>
2020-04-04 23:30:28 -0300

    add transfer method and adjusts deposit method

--- lib/transaction.ex
+++ lib/transaction.ex
@@ -23,12 +23,12 @@ defmodule StnAccount.Transaction do
     ## Examples
 
       iex> account = StnAccount.create("85718312036")
-      iex> new_balance = StnAccount.Transaction.deposit(50, account)
+      iex> new_balance = StnAccount.Transaction.deposit(account, 50)
       iex> new_balance.balance
       %Money{amount: 50, currency: :BRL}
 
     """
-    def deposit(amount, account) do
+    def deposit(account, amount) do
       unless amount > 0 do
         raise(ArgumentError, message: "The amount to deposit should be higher than zero.")
       end
@@ -38,5 +38,40 @@ defmodule StnAccount.Transaction do
       account
       |> Map.put(:balance, Money.add(account.balance, transaction.amount))
       |> Map.put(:transactions, account.transactions ++ [transaction])
+    end
+
+    @doc """
+      Transfers money from `origin_account` to `dest_account` if `origin_account`
+      has enough funds to transfer.
+
+      ## Examples
+
+        iex> account_origin = StnAccount.create_and_deposit("85718312036", 500)
+        iex> account_dest = StnAccount.create("98310751028")
+        iex> account_dest = StnAccount.Transaction.transfer(account_origin, account_dest, 50)
+        iex> account_dest.balance
+        %Money{amount: 50, currency: :BRL}
+
+    """
+    def transfer(origin_account, dest_account, amount) do
+      unless amount > 0 do
+        raise(ArgumentError, message: "The amount to deposit should be higher than zero.")
+      end
+
+      unless StnAccount.get_balance(origin_account) > amount do
+        raise(ArgumentError, message: "Not enough funds to transfer.")
+      end
+
+      origin_transaction = create_transaction(-amount)
+      dest_transaction = create_transaction(amount)
+
+      origin_account
+      |> Map.put(:balance, Money.add(origin_account.balance, origin_transaction.amount))
+      |> Map.put(:transactions, origin_account.transactions ++ [origin_transaction])
+
+      dest_account
+      |> Map.put(:balance, Money.add(dest_account.balance, dest_transaction.amount))
+      |> Map.put(:transactions, dest_account.transactions ++ [dest_transaction])
+
     end
 end
