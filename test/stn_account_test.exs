1135e144bd7847dd6f0b5e318bdf3aa033dcb78a
Alexandre Mutti <alexmutti@gmail.com>
2020-04-07 12:29:06 -0300

    add test to StnAccount functions

--- test/stn_account_test.exs
+++ test/stn_account_test.exs
@@ -2,4 +2,19 @@ defmodule StnAccountTest do
   use ExUnit.Case
   doctest StnAccount
 
+
+  # StnAccount.create(tax_id) tests:
+  test "tax_id as nil" do
+    assert_raise(ArgumentError, fn -> StnAccount.create(nil) end)
+  end
+
+  test "tax_id invalid" do
+    assert_raise(ArgumentError, fn -> StnAccount.create("8571831203a") end)
+  end
+
+  #StnAccount.get_balance
+  test "inputs an invalid map (account)" do
+    assert_raise(ArgumentError, fn -> StnAccount.get_balance(%{"account": "someacc"}) end)
+  end
+
 end
